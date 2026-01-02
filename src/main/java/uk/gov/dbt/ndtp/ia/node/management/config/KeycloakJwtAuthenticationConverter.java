878f6330028b515a58d852d2f8502af87605b189
nikan Negaresh <nikan.negaresh@informed.com>
2026-01-02 13:58:32 +0000

    Remove client secret dependency in Keycloak JWT converter and update resource configurations

--- src/main/java/uk/gov/dbt/ndtp/ia/node/management/config/KeycloakJwtAuthenticationConverter.java
+++ src/main/java/uk/gov/dbt/ndtp/ia/node/management/config/KeycloakJwtAuthenticationConverter.java
@@ -1,6 +1,6 @@
 /*
  * SPDX-License-Identifier: Apache-2.0
- * © Crown Copyright 2025. This work has been developed by the National Digital Twin Programme and is legally
+ * © Crown Copyright 2026. This work has been developed by the National Digital Twin Programme and is legally
  * attributed to the Department for Business and Trade (UK) as the governing entity.
  */
 
@@ -69,7 +69,6 @@ public class KeycloakJwtAuthenticationConverter implements Converter<Jwt, Abstra
 
     // Form data keys
     private static final String FORM_CLIENT_ID = "client_id";
-    private static final String FORM_CLIENT_SECRET = "client_secret";
     private static final String FORM_TOKEN = "token";
 
     private final JwtGrantedAuthoritiesConverter defaultGrantedAuthoritiesConverter =
@@ -79,12 +78,6 @@ public class KeycloakJwtAuthenticationConverter implements Converter<Jwt, Abstra
     @Value("${spring.security.oauth2.resourceserver.opaquetoken.introspection-uri}")
     private String introspectionUri;
 
-    @Value("${spring.security.oauth2.resourceserver.opaquetoken.client-id}")
-    private String clientId;
-
-    @Value("${spring.security.oauth2.resourceserver.opaquetoken.client-secret}")
-    private String clientSecret;
-
     /**
      * Performs token introspection by making an HTTP call to the introspection endpoint.
      *
@@ -92,7 +85,7 @@ public class KeycloakJwtAuthenticationConverter implements Converter<Jwt, Abstra
      * @return JwtToken containing the introspection data
      * @throws TokenIntrospectionException If the introspection request fails or returns invalid data
      */
-    private JwtToken performTokenIntrospection(String tokenValue) throws TokenIntrospectionException {
+    private JwtToken performTokenIntrospection(String tokenValue,String clientId) throws TokenIntrospectionException {
         try {
             // Prepare headers for the introspection request
             HttpHeaders headers = new HttpHeaders();
@@ -101,7 +94,6 @@ public class KeycloakJwtAuthenticationConverter implements Converter<Jwt, Abstra
             // Prepare form data for the introspection request
             MultiValueMap<String, String> formData = new LinkedMultiValueMap<>();
             formData.add(FORM_CLIENT_ID, clientId);
-            formData.add(FORM_CLIENT_SECRET, clientSecret);
             formData.add(FORM_TOKEN, tokenValue);
 
             // Create the request entity
@@ -135,10 +127,10 @@ public class KeycloakJwtAuthenticationConverter implements Converter<Jwt, Abstra
     @Override
     public AbstractAuthenticationToken convert(Jwt jwt) {
         try {
-            log.debug("Converting JWT to authentication token");
-
+            String client_id = jwt.getClaimAsString(CLAIM_AZP);
+            log.debug("Converting JWT to authentication token:{}", client_id);
             // Perform token introspection
-            JwtToken introspectionData = performTokenIntrospection(jwt.getTokenValue());
+            JwtToken introspectionData = performTokenIntrospection(jwt.getTokenValue(),client_id);
 
             // Extract authorities from the introspection data
             Collection<GrantedAuthority> authorities = extractAuthoritiesFromIntrospection(introspectionData);
